e727b08ca23d148d0fd39aefcdfb814e02ad0ebd
SSOL <msseock@gmail.com>
2026-04-22 23:52:41 +0900

    xcode 프로젝트 파일 생성

---
+++ YumPick/YumPick/YumPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumPickApp.swift
+//  YumPick
+//
+//  Created by 석민솔 on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct YumPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
