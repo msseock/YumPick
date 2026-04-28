145d0633f26a798ac4287313aa03ca07d2090744
SSOL <msseock@gmail.com>
2026-04-28 15:56:09 +0900

    FCM push notification 수신할 수 있도록 구현

---
+++ YumPick/YumPick/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  YumPick
+//
+//  Created by 석민솔 on 4/28/26.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      if #available(iOS 10.0, *) {
+          UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) { granted, error in
+              print("UNUserNotificationCenter Permission granted: \(granted)")
+          }
+          UNUserNotificationCenter.current().delegate = self
+          Messaging.messaging().delegate = self
+      }
+      application.registerForRemoteNotifications()
+    return true
+  }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([[.banner, .list, .sound]])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        NotificationCenter.default.post(name: Notification.Name("didReceiveRemoteNotification"), object: nil, userInfo: userInfo)
+        completionHandler()
+    }
+    
+    @objc func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+}
--- YumPick/YumPick/YumPickApp.swift
+++ YumPick/YumPick/YumPickApp.swift
@@ -1,8 +1,10 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct YumPickApp: App {
     @State private var authSession = AuthSession()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
