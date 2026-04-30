5e568be43caedfeb5d93893c728d8de101bf43f5
SSOL <msseock@gmail.com>
2026-05-01 01:36:54 +0900

    Portone SDK UIKit 래퍼뷰 구현

---
+++ YumPick/YumPick/Features/Checkout/IamportPaymentView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import iamport_ios
+
+struct IamportPaymentView: UIViewControllerRepresentable {
+    let payment: IamportPayment
+    let onResult: (IamportResponse?) -> Void
+
+    func makeUIViewController(context: Context) -> IamportPaymentViewController {
+        IamportPaymentViewController(payment: payment, onResult: onResult)
+    }
+
+    func updateUIViewController(_ uiViewController: IamportPaymentViewController, context: Context) {}
+}
+
+final class IamportPaymentViewController: UIViewController {
+    private let payment: IamportPayment
+    private let onResult: (IamportResponse?) -> Void
+
+    init(payment: IamportPayment, onResult: @escaping (IamportResponse?) -> Void) {
+        self.payment = payment
+        self.onResult = onResult
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) { fatalError() }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        Iamport.shared.payment(
+            viewController: self,
+            userCode: SecretConstants.impUserCode,
+            payment: payment
+        ) { [weak self] response in
+            self?.onResult(response)
+        }
+    }
+}
