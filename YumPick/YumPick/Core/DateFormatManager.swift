d6cff73984501be160d7fd705dbdd6675c7c073b
SSOL <msseock@gmail.com>
2026-05-05 22:28:59 +0900

    fix: 채팅 날짜 포맷 UTC 처리 수정

--- YumPick/YumPick/Core/DateFormatManager.swift
+++ YumPick/YumPick/Core/DateFormatManager.swift
@@ -29,7 +29,7 @@ final class DateFormatManager {
     private let chatISOFormatter: DateFormatter = {
         let f = DateFormatter()
         f.locale = Locale(identifier: "en_US_POSIX")
-        f.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        f.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
         f.timeZone = TimeZone(identifier: "UTC")
         return f
     }()
